75d8b03a09f38be89ece5590afdd588a18c27eb8
Aurélien <au2001sfg@gmail.com>
2020-06-06 15:27:23 +0200

    Create css and js folders

--- js/global.js
+++ js/global.js
@@ -92,6 +92,55 @@ async function getRandomCoins(limit = 3) {
 function createCoinCard(coin) {
 	const element = document.createElement("div");
 
+	// {
+	// 	"id": 1,
+	// 	"slug": "bitcoin-btc",
+	// 	"symbol": "BTC",
+	// 	"name": "Bitcoin",
+	// 	"description": "Bitcoin is the first decentralized digital currency.",
+	// 	"color": "#f7931A",
+	// 	"iconType": "vector",
+	// 	"iconUrl": "https://cdn.coinranking.com/Sy33Krudb/btc.svg",
+	// 	"websiteUrl": "https://bitcoin.org",
+	// 	"socials": [
+	// 		{
+	//	 		"name": "Bitcoin",
+	//	 		"url": "https://www.reddit.com/r/Bitcoin/",
+	//	 		"type": "reddit"
+	// 		}
+	// 	],
+	// 	"confirmedSupply": true,
+	// 	"type": "coin",
+	// 	"volume": 6818750000,
+	// 	"marketCap": 159393904304,
+	// 	"price": "9370.9993109108",
+	// 	"circulatingSupply": 17009275,
+	// 	"totalSupply": 21000000,
+	// 	"firstSeen": 1330214400000,
+	// 	"change": -0.52,
+	// 	"rank": 1,
+	// 	"numberOfMarkets": 9800,
+	// 	"numberOfExchanges": 190,
+	// 	"history": [
+	// 		"9515.0454185372",
+	// 		"9540.1812284677",
+	// 		"9554.2212643043",
+	// 		"9593.571539283",
+	// 		"9592.8596962985",
+	// 		"9562.5310295967",
+	// 		"9556.7860427046",
+	// 		"9388.823394515",
+	// 		"9335.3004209165",
+	// 		"9329.4331700521",
+	// 		"9370.9993109108"
+	// 	],
+	// 	"allTimeHigh": {
+	// 		"price": "19500.471361532",
+	// 		"timestamp": 1513555200000
+	// 	},
+	// 	"penalty": false
+	// }
+
 	element.innerText = `Name: ${coin.name} – Symbol: ${coin.symbol}`; // TODO
 
 	return element;
