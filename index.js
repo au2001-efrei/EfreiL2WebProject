531c4586f708651e872e839dab958d9f65ad4722
Aurélien <au2001sfg@gmail.com>
2020-06-06 15:11:56 +0200

    Small fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 (async function() {
 	const [ random, up, down ] = await Promise.all([getRandomCoins(), getUp(), getDown()]);
 
-	document.querySelector("#content").classList.remove("hidden");
 	fillCoins(document.querySelector("#random-coins"), random.coins);
 	fillCoins(document.querySelector("#up-coins"), up.coins);
 	fillCoins(document.querySelector("#down-coins"), down.coins);
 	document.querySelector("#loading").classList.add("hidden");
+	document.querySelector("#content").classList.remove("hidden");
 })().catch(console.error);
