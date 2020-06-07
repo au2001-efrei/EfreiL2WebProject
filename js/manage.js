b738417e8807f9703853e0d24b60a07c849fe0c9
Aurélien <au2001sfg@gmail.com>
2020-06-07 17:28:16 +0200

    Create missing pages

---
+++ js/manage.js
@@ -0,0 +1,6 @@
+(async function() {
+	// TODO
+
+	document.querySelector("#loading").classList.add("hidden");
+	document.querySelector("#content").classList.remove("hidden");
+})().catch(console.error);
