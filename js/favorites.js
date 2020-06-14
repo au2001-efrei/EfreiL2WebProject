4967770f95bbea21605d03895ca6a5c0f690d6bf
Aurélien <au2001sfg@gmail.com>
2020-06-14 20:15:43 +0200

    Fix quotes coherence

--- js/favorites.js
+++ js/favorites.js
@@ -5,7 +5,7 @@
 	if (favorites.coins.length > 0) {
 		fillCoins(favoriteCoins, favorites.coins);
 	} else {
-		favoriteCoins.innerText = "You don't have any favorite cryptocurrency yet. Switch to the « Let’s play! » tab to start adding some.";
+		favoriteCoins.innerText = "You don't have any favorite cryptocurrency yet. Switch to the “Let’s play!” tab to start adding some.";
 	}
 
 	document.querySelector("#loading").classList.add("hidden");
