66a4be93aa0d71f43ce039c5340a0f0cb93962f1
Aurélien <au2001sfg@gmail.com>
2020-06-14 20:08:25 +0200

    Create favorites page

--- js/favorites.js
+++ js/favorites.js
@@ -1,5 +1,12 @@
 (async function() {
-	// TODO
+	const favoriteCoins = document.querySelector("#favorite-coins");
+
+	const favorites = await getFavorites();
+	if (favorites.coins.length > 0) {
+		fillCoins(favoriteCoins, favorites.coins);
+	} else {
+		favoriteCoins.innerText = "You don't have any favorite cryptocurrency yet. Switch to the « Let’s play! » tab to start adding some.";
+	}
 
 	document.querySelector("#loading").classList.add("hidden");
 	document.querySelector("#content").classList.remove("hidden");
